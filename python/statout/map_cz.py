6421ca199a5791d787d9ecb2eb3058b2ff3b058b
Jiří Petříček <jiri.petricek.jiri@gmail.com>
2026-07-01 00:00:00 +0200

    Fix the gap in choropleth title. [path-limited from 9b4603c778c3dc5a9fb88829feade2fde804958f]

--- python/statout/map_cz.py
+++ python/statout/map_cz.py
@@ -234,6 +234,7 @@ def choropleth_cz(
     if title:
         fig.subplots_adjust(top=0.88)
         fig._subplots_adjust_kwargs = {"top": 0.88}
+        fig._suptitle_gap_pt = 11
         fig.suptitle(
             title,
             fontsize=plt.rcParams.get("axes.titlesize", 9),
